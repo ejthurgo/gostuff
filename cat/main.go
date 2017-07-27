21c1a56888cd629cd67bb98b4f5f00e5b434cc5f
Edward Thurgood <ejthurgo@gmail.com>
2017-07-27 15:16:34 -0700

    Update

--- cat/main.go
+++ cat/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
@@ -25,14 +24,17 @@ func main() {
 		out = os.Stderr
 	}
 
-	r := io.TeeReader(file, out)
-	b, err := ioutil.ReadAll(r)
+	//r := io.TeeReader(file, out)
+	//b, err := ioutil.ReadAll(r)
+	//l := len(b)
+
+	l, err := io.Copy(out, file)
 	if err != nil {
 		panic("Unable to read file.")
 	}
 
 	if *v {
-		fmt.Printf("%d bytes read.\n", len(b))
+		fmt.Printf("%d bytes read.\n", l)
 	}
 	fmt.Println("🐱  🐱  🐱  🐱  🐱  🐱  Done  🐱  🐱  🐱  🐱  🐱  🐱")
 
